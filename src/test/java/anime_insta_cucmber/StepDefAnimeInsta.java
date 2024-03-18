c1500468e1b5cae913e7d4a08f477f725a094c1b
FadBiao <farid.mbiao@gmail.com>
2024-03-18 22:46:50 +0100

    test creer compte; okay

--- src/test/java/anime_insta_cucmber/StepDefAnimeInsta.java
+++ src/test/java/anime_insta_cucmber/StepDefAnimeInsta.java
@@ -10,7 +10,7 @@ public class StepDefAnimeInsta {
 
     private Anime anime;
     private Personnage perso;
-    private int calculatedAge;
+    private String username;
 
     @Given("un personnage {string} de l'anime {string} créé en {int} âgé de {int} ans et ayant une force de {int}")
     public void un_personnage_de_l_anime_créé_en_âgé_de_ans_et_ayant_une_force_de(String perso_name, String anime_titre, Integer anneeCreation, Integer age, Integer force) {
@@ -22,13 +22,13 @@ public class StepDefAnimeInsta {
 
     @When("le personnage crée un compte Instagram avec le username {string}")
     public void le_personnage_crée_un_compte_instagram_avec_le_username(String usernane) {
-
+         perso.creeCompte(username);
+         this.username = perso.getCompte().getUsername();
     }
 
     @Then("le compte Instagram est créé avec le username {string} et associé au personnage {string}")
-    public void le_compte_instagram_est_créé_avec_le_username_et_associé_au_personnage(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void le_compte_instagram_est_créé_avec_le_username_et_associé_au_personnage(String expected_user_name, String perso_name) {
+        assertEquals(expected_user_name, username);
     }
 
 }
